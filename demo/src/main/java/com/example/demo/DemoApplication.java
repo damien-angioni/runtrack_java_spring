cf10a66ffbe020f1cdc31f22e751d567c6d5adf8
damien-angioni <115216654+damien-angioni@users.noreply.github.com>
2023-10-16 10:29:45 +0200

    Spring Init
    Spring project blank

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import controller.HelloController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
